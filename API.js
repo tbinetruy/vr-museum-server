0e50415835ea08646efe2ea4780c4e6aceee3908
Thomas Binetruy <tbinetruy@gmail.com>
2018-08-02 15:01:39 -0400

    setup basic server

--- API.js
+++ API.js
@@ -4,3 +4,6 @@ const app = express()
 app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+app.use(express.static('public'))
+
